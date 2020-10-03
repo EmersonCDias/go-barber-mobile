28261dca5c84988bf54cee1e82ebac553c8182f6
EmersonCDias <emerson_cdias@hotmail.com>
2020-10-03 18:38:15 -0300

    Creating SignIn Screen

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,17 +1,53 @@
 import React from "react";
-import { Image } from 'react-native';
+import { ScrollView, Image, View, KeyboardAvoidingView, Platform } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
-import { Container, Title } from './styles';
+import Input from '../../componets/Input';
+import Button from '../../componets/Button';
+import { Container, Title, ForgotPassword, ForgotPasswordText, CreateAccountButton, CreateAccountButtonText } from './styles';
 import logoImg from '../../assets/logo.png'
+
 
 const SignIn = () => {
   return (
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView contentContainerStyle={{ flex: 1 }} keyboardShouldPersistTaps="handled">
+          <Container>
+            <Image source={logoImg} />
 
-    <Container>
-      <Image source={logoImg} />
+            <View>
+              <Title>Faça seu logon</Title>
+            </View>
 
-      <Title>Faça seu logon</Title>
-    </Container>
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="password" icon="lock" placeholder="Senha" />
+
+            <Button onPress={() => { }}>Entrar</Button>
+
+            <ForgotPassword onPress={() => { }} >
+              <ForgotPasswordText>
+                Esqueci minha senha
+            </ForgotPasswordText>
+            </ForgotPassword>
+          </Container>
+        </ScrollView>
+
+        <CreateAccountButton onPress={() => { }}>
+          <Icon name="log-in" size={20} color="#ff9000" />
+
+          <View>
+            <CreateAccountButtonText>
+              Criar conta
+            </CreateAccountButtonText>
+          </View>
+        </CreateAccountButton>
+      </KeyboardAvoidingView>
+    </>
   )
 }
 

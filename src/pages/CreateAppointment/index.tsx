6fc081e401cebb8cc39b76fc7ba3859af777200d
EmersonCDias <emerson_cdias@hotmail.com>
2021-02-13 17:16:26 -0300

    Creating appointment

--- src/pages/CreateAppointment/index.tsx
+++ src/pages/CreateAppointment/index.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useEffect, useMemo, useState } from 'react';
-import { Platform } from 'react-native';
+import { Platform, Alert } from 'react-native';
 import { useNavigation, useRoute } from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/Feather';
 import DateTimePicker from '@react-native-community/datetimepicker';
@@ -27,6 +27,8 @@ import {
   SectionContent,
   Hour,
   HourText,
+  CreateAppointmentButton,
+  CreateAppointmentButtonText,
 } from './styles';
 import { RouteParams, Provider, AvailabilityItem } from './types';
 import { useAuth } from '../../hooks/AuthHook';
@@ -52,7 +54,7 @@ const CreateAppointment = () => {
 
   const { user } = useAuth();
 
-  const { goBack } = useNavigation();
+  const { goBack, navigate } = useNavigation();
 
   const navigateBack = useCallback(goBack, [goBack]);
 
@@ -80,6 +82,24 @@ const CreateAppointment = () => {
     (hour: number) => setSelectedHour(hour),
     [selectedHour],
   );
+
+  const handleCreateAppointment = useCallback(async () => {
+    try {
+      const date = new Date(selectedDate);
+
+      date.setHours(selectedHour);
+      date.setMinutes(0);
+
+      await api.post('appointments', {
+        provider_id: selectedProvider,
+        date,
+      });
+
+      navigate('AppointmentCreated', { date: date.getTime() });
+    } catch (err) {
+      Alert.alert('Erro ao criar agendamento', 'Tente novamente');
+    }
+  }, [navigate, selectedDate, selectedHour, selectedProvider]);
 
   const handleDateChange = useCallback((event: any, date: Date | undefined) => {
     if (Platform.OS === 'android') setShowDatePicker(false);
@@ -149,8 +169,8 @@ const CreateAppointment = () => {
   }, [selectedDate, selectedProvider]);
 
   return (
-    <Content>
-      <Container>
+    <Container>
+      <Content>
         <Header>
           <BackButton onPress={navigateBack}>
             <Icon name="chevron-left" size={24} color="#999591" />
@@ -250,8 +270,12 @@ const CreateAppointment = () => {
             </SectionContent>
           </Section>
         </Schedule>
-      </Container>
-    </Content>
+
+        <CreateAppointmentButton onPress={handleCreateAppointment}>
+          <CreateAppointmentButtonText>Agendar</CreateAppointmentButtonText>
+        </CreateAppointmentButton>
+      </Content>
+    </Container>
   );
 };
 
--- src/pages/CreateAppointment/styles.ts
+++ src/pages/CreateAppointment/styles.ts
@@ -133,3 +133,18 @@ export const HourText = styled.Text<HourTextProps>`
   font-family: 'RobotSlab-Regular';
   font-size: 16px;
 `;
+
+export const CreateAppointmentButton = styled(RectButton)`
+  height: 50px;
+  background: #ff9000;
+  border-radius: 10px;
+  align-items: center;
+  justify-content: center;
+  margin: 0 24px 24px;
+`;
+
+export const CreateAppointmentButtonText = styled.Text`
+  font-family: 'RobotSlab-Medium';
+  font-size: 16px;
+  color: #232129;
+`;
